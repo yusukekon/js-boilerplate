bffba179c097643f0e818676152b93a15b713154
yusukekon <kkoonn84@gmail.com>
2013-11-16 23:36:14 +0900

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jshint: {
+            files: [
+                'src/**/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        gjslint: {
+            options: {
+                flags: ['--flagfile .gjslintrc'],
+                reporter: {
+                    name: 'console'
+                }
+            },
+            all: {
+                src: ['src/**/*.js']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-gjslint');
+
+    grunt.registerTask('default', ['jshint', 'gjslint:all']);
+};
